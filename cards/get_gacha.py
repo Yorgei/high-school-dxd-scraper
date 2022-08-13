47be7835a6f7b67dca0c882554f0de2a0be294b1
Yorgei <riasisbest1@gmail.com>
2022-08-14 00:16:11 +1000

    add check

--- cards/get_gacha.py
+++ cards/get_gacha.py
@@ -1,8 +1,10 @@
 import requests
 import time
 import json
+
 with open('cards/cookies.json', 'r') as cookie_file:
     cookie_data = json.load(cookie_file)
+
 cookies = {
     'x-mbga-check-cookie': '1',
     'sp_mbga_sid_12014827': cookie_data['cookie'],
@@ -12,10 +14,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
-    # 'Accept-Encoding': 'gzip, deflate',
     'Connection': 'keep-alive',
     'Referer': 'http://g12014827.sp.pf.mbga.jp/?url=http%3A%2F%2Fmg.highschooldd.net%2Fsp%2Fmypage.html',
-    # Requests sorts cookies= alphabetically
     'Upgrade-Insecure-Requests': '1',
 }
 
@@ -23,8 +23,15 @@ params = {
     'url': 'http://mg.highschooldd.net/sp/gacha',
 }
 
-with open('dump.json', 'r') as json_file:
-    json_data = json.load(json_file)
+try:
+    with open('dump.json', 'r') as json_file:
+        json_data = json.load(json_file)
+except FileNotFoundError:
+    print('List not found. Run get_base_ids.py to scrape list')
+    exit()
+except Exception as e:
+    print('Unknown error', e)
+    exit()
 
 # remove url from card ids
 card_list = [x.replace('http://cdn-prod.highschooldd.net/sp/image/cards/C/', '').replace('.png', '') for x in json_data]
